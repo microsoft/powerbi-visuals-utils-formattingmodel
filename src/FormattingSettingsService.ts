2b68946396460f9e0b20bc08a267a865f087938f
Iuliia Kulagina <v-ikulagina@microsoft.com>
2024-04-03 10:08:40 +0200

    Fix eslint issues

--- src/FormattingSettingsService.ts
+++ src/FormattingSettingsService.ts
@@ -1,6 +1,6 @@
 import powerbi from "powerbi-visuals-api";
 import { formattingSettings } from ".";
-import { Cards, CardGroupEntity, CompositeCard, Group, Model, SimpleCard, Slice } from "./FormattingSettingsComponents";
+import { Cards, CardGroupEntity, CompositeCard, Model, SimpleCard, Slice } from "./FormattingSettingsComponents";
 import { IBuildFormattingSlicesParams, IFormattingSettingsService } from "./FormattingSettingsInterfaces";
 
 import visuals = powerbi.visuals;
@@ -20,9 +20,9 @@ export class FormattingSettingsService implements IFormattingSettingsService {
      * @returns visual formatting settings model 
      */
     public populateFormattingSettingsModel<T extends Model>(typeClass: new () => T, dataView: powerbi.DataView): T {
-        let defaultSettings: T = new typeClass();
+        const defaultSettings: T = new typeClass();
 
-        let dataViewObjects = dataView?.metadata?.objects;
+        const dataViewObjects = dataView?.metadata?.objects;
         if (dataViewObjects) {
             // loop over each formatting property and set its new value if exists
             defaultSettings.cards?.forEach((card: Cards) => {
@@ -54,14 +54,14 @@ export class FormattingSettingsService implements IFormattingSettingsService {
      * @returns powerbi visual formatting model
      */
     public buildFormattingModel(formattingSettingsModel: Model): visuals.FormattingModel {
-        let formattingModel = {
+        const formattingModel = {
             cards: []
         }
         
         formattingSettingsModel.cards
             .filter(({visible = true}) => visible)
             .forEach((card: Cards) => {
-                let formattingCard: visuals.FormattingCard = {
+                const formattingCard: visuals.FormattingCard = {
                     displayName: (this.localizationManager && card.displayNameKey) ? this.localizationManager.getDisplayName(card.displayNameKey) : card.displayName,
                     description: (this.localizationManager && card.descriptionKey) ? this.localizationManager.getDisplayName(card.descriptionKey) : card.description,
                     groups: [],
@@ -72,7 +72,7 @@ export class FormattingSettingsService implements IFormattingSettingsService {
                 const objectName = card.name;
 
                 if (card.topLevelSlice) {
-                    let topLevelToggleSlice: visuals.FormattingSlice = card.topLevelSlice.getFormattingSlice(objectName, this.localizationManager);
+                    const topLevelToggleSlice: visuals.FormattingSlice = card.topLevelSlice.getFormattingSlice(objectName, this.localizationManager);
                     topLevelToggleSlice.suppressDisplayName = true;
                     formattingCard.topLevelToggle = (<visuals.EnabledSlice>topLevelToggleSlice);
                 }
@@ -124,7 +124,7 @@ export class FormattingSettingsService implements IFormattingSettingsService {
                                 // Build formatting container item object
                                 const containerIemName = containerItem.displayNameKey ? containerItem.displayNameKey : containerItem.displayName;
                                 const containerItemUid: string = containerUid + containerIemName;
-                                let formattingContainerItem: visuals.FormattingContainerItem = {
+                                const formattingContainerItem: visuals.FormattingContainerItem = {
                                     displayName: (this.localizationManager && containerItem.displayNameKey)
                                         ? this.localizationManager.getDisplayName(containerItem.displayNameKey) : containerItem.displayName,
                                     slices: [],
@@ -141,7 +141,7 @@ export class FormattingSettingsService implements IFormattingSettingsService {
 
                         if (cardGroupInstance.slices) {
                             if (cardGroupInstance.topLevelSlice) {
-                                let topLevelToggleSlice: visuals.FormattingSlice = cardGroupInstance.topLevelSlice.getFormattingSlice(objectName, this.localizationManager);
+                                const topLevelToggleSlice: visuals.FormattingSlice = cardGroupInstance.topLevelSlice.getFormattingSlice(objectName, this.localizationManager);
                                 topLevelToggleSlice.suppressDisplayName = true;
                                 (formattingGroup.displayName==undefined ? formattingCard : formattingGroup).topLevelToggle = (<visuals.EnabledSlice>topLevelToggleSlice);
                             }
@@ -163,7 +163,7 @@ export class FormattingSettingsService implements IFormattingSettingsService {
         // Filter slices based on their visibility
         slices?.filter(({visible = true}) => visible)
             .forEach((slice: Slice) => {
-            let formattingSlice: visuals.FormattingSlice = slice?.getFormattingSlice(objectName, this.localizationManager);
+            const formattingSlice: visuals.FormattingSlice = slice?.getFormattingSlice(objectName, this.localizationManager);
 
             if (formattingSlice) {
                 // Modify formatting slice uid if needed
@@ -183,10 +183,11 @@ export class FormattingSettingsService implements IFormattingSettingsService {
         // Proceeded slice names are saved to prevent duplicated default descriptors in case of using 
         // formatting categories & selectors, since they have the same descriptor objectName and propertyName
         const sliceNames: { [name: string]: boolean } = {};
-        let revertToDefaultDescriptors: visuals.FormattingDescriptor[] = [];
+        const revertToDefaultDescriptors: visuals.FormattingDescriptor[] = [];
         let cardSlicesDefaultDescriptors: visuals.FormattingDescriptor[]
         let cardContainerSlicesDefaultDescriptors: visuals.FormattingDescriptor[] = [];
 
+        // eslint-disable-next-line
         if (card instanceof CompositeCard && card.topLevelSlice) revertToDefaultDescriptors.push(...card.topLevelSlice?.getRevertToDefaultDescriptor(card.name));
 
         const cardGroupInstances = <CardGroupEntity[]>(card instanceof SimpleCard ? 
